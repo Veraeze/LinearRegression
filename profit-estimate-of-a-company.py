bd5ca1b0faa76dea10d2a218438066eb37dd10d0
Veraeze <Veraeze18@gmail.com>
2025-06-03 13:52:01 +0100

    split the dataset into the training set and test set

--- profit-estimate-of-a-company.py
+++ profit-estimate-of-a-company.py
@@ -6,25 +6,26 @@ from numpy.ma.core import remainder
 
 #loading and defining the data
 companies = pd.read_csv('1000_Companies.csv')
-x = companies.iloc[:, :-1]
+X = companies.iloc[:, :-1]
 y = companies.iloc[:, 4]
 # print(companies.head())
 
 #data visualization
 sns.heatmap(companies.corr(numeric_only=True), annot=True, cmap='coolwarm')
 plt.title('correlation heatmap')
-# plt.show()
+plt.show()
 
 #encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
-label_encoder = LabelEncoder()
-companies['State'] = label_encoder.fit_transform(companies['State'])
-print(companies.head())
-
 column_transformer = ColumnTransformer(
-                    transformers = [('encoder', OneHotEncoder(), [3])],
+                    transformers = [('encoder', OneHotEncoder(drop='first', sparse_output=False), ['State'])],
                     remainder = 'passthrough')
-x = column_transformer.fit_transform(x)
-print(x)
+X = column_transformer.fit_transform(X)
+print(X)
+
+# splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
